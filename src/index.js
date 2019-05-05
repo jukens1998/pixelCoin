e6d6413d2f04d76b0f872ab94bd7c8ad17fc264f
root <juandiemore@gmail.com>
2019-05-05 05:00:51 -0500

    el commit antes de dormir :)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const port = 3000;
 const express = require('express');
 const app = express();
+const indRoutes = require('./routes/index');
 
 
 //app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
-const indRoutes = app.use(require('./routes/index'));
-
+app.use(indRoutes);
 
 app.listen(port,()=>{
   console.log("Servidor corriendo en el puerto: ",port)
 });
+ 
